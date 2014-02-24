4a45f517193592b1ffdd58cc61958720da3886fd
Jonathan_air <Jonathan@Jonathan-airdeMacBook-Air.local>
2014-02-24 09:33:02 +0800

    update comment

--- scope/main.c
+++ scope/main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-int num =50;
+int num =50; // Global variable
 int main(void){
 
 	printf("the numbie is %d\n", num);
-	int x = 0;
+	int x = 0; // Local variable
 	int num=10;
 	for (;x<2;x++) {
 		num = num + x;
