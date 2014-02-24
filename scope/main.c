f03b3feb18b2007f05ee978db06113cae779b173
Jonathan_air <Jonathan@Jonathan-airdeMacBook-Air.local>
2014-02-24 09:48:42 +0800

    Block variable test

--- scope/main.c
+++ scope/main.c
@@ -7,8 +7,10 @@ int main(void){
 	int x = 0; // Local variable
 	int num=10;
 	for (;x<2;x++) {
+		int num = 100; // Block variable
 		num = num + x;
 		printf("the number in for loop is %d\n",num);
 	}
+	printf("the local variable is %d\n", num);
 	return 0;
 }
