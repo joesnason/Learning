035b41091f793c4941f93af1d8ac716ababc3e19
Jonathan_air <Jonathan@Jonathan-airdeMacBook-Air.local>
2014-03-04 18:48:16 +0800

    try to include a c file and to call his function.

--- preprocessor/main.c
+++ preprocessor/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "main.h"
 
+#include "another.c"
+
 int main(void){
 
 	my_printf();
@@ -10,5 +12,11 @@ int main(void){
 int my_printf(void){
 
 	printf("Hello World\n");
+
+	int x;
+
+	x = my_add(3,5);
+
+	printf("get number: %d\n",x);
 	return 0;
 }
