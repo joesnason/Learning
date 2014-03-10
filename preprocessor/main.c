92ecf0fe31cf151bb19c3a41d128b769a735150c
Jonathan_air <Jonathan@Jonathan-airdeMacBook-Air.local>
2014-03-10 18:39:34 +0800

    test #define

--- preprocessor/main.c
+++ preprocessor/main.c
@@ -2,6 +2,8 @@
 #include "main.h"
 
 #include "another.h"
+#define MAXNUMBER 200;
+
 
 int main(void){
 
@@ -14,9 +16,11 @@ int my_printf(void){
 	printf("Hello World\n");
 
 	int x;
+	int num = MAXNUMBER;
 
 	x = my_add(3,5);
 
 	printf("get number: %d\n",x);
+	printf("get max number: %d\n", num);
 	return 0;
 }
