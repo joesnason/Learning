7aeb1962f2619084e8e0bb5ecb74b584c4064511
Jonathan_air <Jonathan@Jonathan-airdeMacBook-Air.local>
2014-03-11 11:07:34 +0800

    define macro test

--- preprocessor/main.c
+++ preprocessor/main.c
@@ -3,7 +3,8 @@
 
 #include "another.h"
 #define MAXNUMBER 200;
-
+#define swap_int(A,B); {int t; \
+							t = A; A = B; B = t	;}
 
 int main(void){
 
@@ -17,10 +18,19 @@ int my_printf(void){
 
 	int x;
 	int num = MAXNUMBER;
+	int big = 100;
+	int small = 10;
+
 
 	x = my_add(3,5);
 
 	printf("get number: %d\n",x);
 	printf("get max number: %d\n", num);
+
+// define macro test 
+	printf("the big number is %d , the small number is %d \n",big,small);
+	swap_int(big,small);
+	printf("after to swap.....\n");
+	printf("the big number is %d , the small number is %d \n",big,small);
 	return 0;
 }
